810b0527a84561f6f1038db65fa89fc8eae3fcf0
Denis Korkhov <sqdron.io@gmail.com>
2016-10-15 17:14:54 +0300

    Refactoring

--- digitalocean/droplet.go
+++ digitalocean/droplet.go
@@ -7,9 +7,3 @@ type dropletApi struct {
 func DropletUnit() *dropletApi {
 	return &dropletApi{}
 }
-
-//func (d *dropletApi) Create() (model.Unit, error){
-//
-//}
-//func (d *dropletApi) List() ([]*model.Unit, error)
-//func (d *dropletApi) Delete(model.Unit) error
--- digitalocean/oauth_test.go
+++ digitalocean/oauth_test.go
@@ -8,12 +8,11 @@ import (
 
 func Test_Authenticate(t *testing.T) {
 	Convey("Authenticate", t, func() {
-		provider := OAuth("digitalocean_key", "secret", "http://test.com/auth", "read", "write")
-		url, err :=provider.GetAccessUrl("test_state")
+		oauth := OAuth("digitalocean_key", "secret", "http://test.com/auth", "read", "write")
+		url, err :=oauth.GetAccessUrl()
 		So(err, ShouldBeNil)
 		So(url, ShouldContainSubstring, "cloud.digitalocean.com/v1/oauth/authorize")
 		So(url, ShouldContainSubstring, fmt.Sprintf("client_id=%s", "digitalocean_key"))
-		So(url, ShouldContainSubstring, "state=test_state")
 		So(url, ShouldContainSubstring, "scope=read")
 	})
 }
